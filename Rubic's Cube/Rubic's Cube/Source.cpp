396d7f8e97b84a1e41c85a1716bd06aa76a3667f
besszerf <caruzzo74@gmail.com>
2017-01-26 22:17:03 +0100

    <feat> Trackback is added.

--- Rubic's Cube/Rubic's Cube/Source.cpp
+++ Rubic's Cube/Rubic's Cube/Source.cpp
@@ -3,7 +3,8 @@
 
 int main(int argc, char* argv[]) {
   Cube mycube;
-
-
+  mycube.draw_cube();
+  for (; mycube.trackback.size(); mycube.undo_last_step());
+  cout << mycube.is_ready();
   return 0;
 }  
