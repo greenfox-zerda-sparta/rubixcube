277e285ff8d32c3c5561c5c0546460d90c83d4ec
Czetenyi Kristof <wekkew@gmail.com>
2017-01-26 16:46:43 +0100

    fixed get_vector_for_lego() missed filling the original vector

--- Rubic's Cube/Rubic's Cube/Source.cpp
+++ Rubic's Cube/Rubic's Cube/Source.cpp
@@ -3,47 +3,7 @@
 
 int main(int argc, char* argv[]) {
   Cube mycube;
-  /*
-  cout << "FRONT ROTATION" << endl;
-  mycube.rotate_front();
-  mycube.rotate_front();
-  mycube.rotate_front();
-  mycube.rotate_front();
-  */
-  mycube.draw_cube();
-  cout << "Is it ready? " << mycube.is_ready() << endl;
 
 
-  cout << "Rotated left." << endl;
-  mycube.rotate_left();
-  cout << "Is it ready? " << mycube.is_ready() << endl;
-
-  cout << "Rotated left 3 times." << endl;
-  mycube.rotate_left();
-  mycube.rotate_left();
-  mycube.rotate_left();
-  cout << "Is it ready? " << mycube.is_ready() << endl;
-  /*
-  cout << "UP ROTATION" << endl;
-  mycube.rotate_up();
-  mycube.draw_cube();
-
-  cout << "Back ROTATION" << endl;
-  mycube.rotate_back();
-  mycube.draw_cube();
-  
-  cout << "Down ROTATION" << endl;
-  mycube.rotate_down();
-  mycube.draw_cube();
-  
-  cout << "Left ROTATION" << endl;
-  mycube.roatate_left();
-  mycube.draw_cube();
-  
-  cout << "Right ROTATION" << endl;
-  mycube.rotate_right
-  mycube.draw_cube();*/
-  
-  
   return 0;
 }  
