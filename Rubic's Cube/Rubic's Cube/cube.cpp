6fffec59ed3202e2bba021ec2ec9fa4799527661
Czetenyi Kristof <wekkew@gmail.com>
2017-01-26 16:26:32 +0100

    updateted to lego's requiest

--- Rubic's Cube/Rubic's Cube/cube.cpp
+++ Rubic's Cube/Rubic's Cube/cube.cpp
@@ -2,7 +2,7 @@
 
 Cube::Cube() {
   srand(time(NULL));
-  //random_shuffle();
+  random_shuffle();
 }
 
 void Cube::random_shuffle() {
@@ -41,15 +41,15 @@ void Cube::random_shuffle() {
 void Cube::fill_faces_to_cube() {
   faces_of_cube.clear();
   faces_of_cube.push_back(front);
-  faces_of_cube.push_back(up);
-  faces_of_cube.push_back(back);
-  faces_of_cube.push_back(down);
   faces_of_cube.push_back(right);
+  faces_of_cube.push_back(back);
   faces_of_cube.push_back(left);
+  faces_of_cube.push_back(up);  
+  faces_of_cube.push_back(down);  
 }
 
 void Cube::rotate_front() {
-  char temp = front[0];
+  int temp = front[0];
   front[0] = front[6];
   front[6] = front[8];
   front[8] = front[2];
@@ -60,9 +60,9 @@ void Cube::rotate_front() {
   front[7] = front[5];
   front[5] = temp;
 
-  char temp0 = up[6];
-  char temp1 = up[7];
-  char temp2 = up[8];
+  int temp0 = up[6];
+  int temp1 = up[7];
+  int temp2 = up[8];
   up[6] = left[8];
   up[7] = left[5];
   up[8] = left[2];
@@ -78,7 +78,7 @@ void Cube::rotate_front() {
 }
 
 void Cube::rotate_up() {
-  char temp = up[0];
+  int temp = up[0];
   up[0] = up[6];
   up[6] = up[8];
   up[8] = up[2];
@@ -89,9 +89,9 @@ void Cube::rotate_up() {
   up[7] = up[5];
   up[5] = temp;
 
-  char temp0 = back[0];
-  char temp1 = back[1];
-  char temp2 = back[2];
+  int temp0 = back[0];
+  int temp1 = back[1];
+  int temp2 = back[2];
   back[0] = left[0];
   back[1] = left[1];
   back[2] = left[2];
@@ -107,7 +107,7 @@ void Cube::rotate_up() {
 }
 
 void Cube::rotate_back() {
-  char temp = back[0];
+  int temp = back[0];
   back[0] = back[6];
   back[6] = back[8];
   back[8] = back[2];
@@ -118,9 +118,9 @@ void Cube::rotate_back() {
   back[7] = back[5];
   back[5] = temp;
 
-  char temp0 = up[2];
-  char temp1 = up[1];
-  char temp2 = up[0];
+  int temp0 = up[2];
+  int temp1 = up[1];
+  int temp2 = up[0];
   up[2] = right[8];
   up[1] = right[5];
   up[0] = right[2];
@@ -136,7 +136,7 @@ void Cube::rotate_back() {
 }
 
 void Cube::rotate_down() {
-  char temp = down[0];
+  int temp = down[0];
   down[0] = down[6];
   down[6] = down[8];
   down[8] = down[2];
@@ -147,9 +147,9 @@ void Cube::rotate_down() {
   down[7] = down[5];
   down[5] = temp;
 
-  char temp0 = front[6];
-  char temp1 = front[7];
-  char temp2 = front[8];
+  int temp0 = front[6];
+  int temp1 = front[7];
+  int temp2 = front[8];
   front[6] = left[6];
   front[7] = left[7];
   front[8] = left[8];
@@ -165,7 +165,7 @@ void Cube::rotate_down() {
 }
 
 void Cube::rotate_right() {
-  char temp = right[0];
+  int temp = right[0];
   right[0] = right[6];
   right[6] = right[8];
   right[8] = right[2];
@@ -176,9 +176,9 @@ void Cube::rotate_right() {
   right[7] = right[5];
   right[5] = temp;
 
-  char temp0 = up[8];
-  char temp1 = up[5];
-  char temp2 = up[2];
+  int temp0 = up[8];
+  int temp1 = up[5];
+  int temp2 = up[2];
   up[8] = front[8];
   up[5] = front[5];
   up[2] = front[2];
@@ -194,7 +194,7 @@ void Cube::rotate_right() {
 }
 
 void Cube::rotate_left() {
-  char temp = left[0];
+  int temp = left[0];
   left[0] = left[6];
   left[6] = left[8];
   left[8] = left[2];
@@ -205,9 +205,9 @@ void Cube::rotate_left() {
   left[7] = left[5];
   left[5] = temp;
 
-  char temp0 = up[0];
-  char temp1 = up[3];
-  char temp2 = up[6];
+  int temp0 = up[0];
+  int temp1 = up[3];
+  int temp2 = up[6];
   up[0] = back[8];
   up[3] = back[5];
   up[6] = back[2];
@@ -224,8 +224,8 @@ void Cube::rotate_left() {
 
 void Cube::draw_cube() {
   fill_faces_to_cube();
-  for (int i = 0; i < faces_of_cube.size(); i++) {
-    for (int j = 0; j < faces_of_cube[i].size(); j++) {
+  for (unsigned int i = 0; i < faces_of_cube.size(); i++) {
+    for (unsigned int j = 0; j < faces_of_cube[i].size(); j++) {
       if (j % 3 == 0) {
         cout << endl;
       }
@@ -245,6 +245,16 @@ bool Cube::is_ready() {
     }
   }
   return true;
+}
+
+vector<int> Cube::get_vector_for_Lego() {
+  vector<int> for_lego;
+  for (unsigned int i = 0; i < faces_of_cube.size(); i++) {
+    for (unsigned int j = 0; j < faces_of_cube[i].size(); j++) {
+      for_lego.push_back(faces_of_cube[i][j]);
+    }
+  }
+  return for_lego;
 }
 
 Cube::~Cube() {
