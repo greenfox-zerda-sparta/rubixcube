7f59360c6ff03c38b1082ef4b0f0cc21c1e88c53
Czetenyi Kristof <wekkew@gmail.com>
2017-01-26 12:25:46 +0100

    rotate_down() finished

--- Rubic's Cube/Rubic's Cube/cube.cpp
+++ Rubic's Cube/Rubic's Cube/cube.cpp
@@ -1,25 +1,25 @@
 #include "cube.h"
 
 Cube::Cube() {
-  faces_of_cube.push_back(back);
+  faces_of_cube.push_back(down);
   faces_of_cube.push_back(up);
-  faces_of_cube.push_back(back);
+  faces_of_cube.push_back(down);
   faces_of_cube.push_back(down);
   faces_of_cube.push_back(right);
   faces_of_cube.push_back(left);
 }
 
 void Cube::rotate_front() {
-  char temp = back[0];
-  back[0] = back[6];
-  back[6] = back[8];
-  back[8] = back[2];
-  back[2] = temp;
-  temp = back[1];
-  back[1] = back[3];
-  back[3] = back[7];
-  back[7] = back[5];
-  back[5] = temp;
+  char temp = down[0];
+  down[0] = down[6];
+  down[6] = down[8];
+  down[8] = down[2];
+  down[2] = temp;
+  temp = down[1];
+  down[1] = down[3];
+  down[3] = down[7];
+  down[7] = down[5];
+  down[5] = temp;
  
   char temp0 = up[6];
   char temp1 = up[7];
@@ -39,16 +39,16 @@ void Cube::rotate_front() {
 }
 
 void Cube::rotate_back() {
-  char temp = back[0];
-  back[0] = back[6];
-  back[6] = back[8];
-  back[8] = back[2];
-  back[2] = temp;
-  temp = back[1];
-  back[1] = back[3];
-  back[3] = back[7];
-  back[7] = back[5];
-  back[5] = temp;
+  char temp = down[0];
+  down[0] = down[6];
+  down[6] = down[8];
+  down[8] = down[2];
+  down[2] = temp;
+  temp = down[1];
+  down[1] = down[3];
+  down[3] = down[7];
+  down[7] = down[5];
+  down[5] = temp;
   char temp0 = up[2];
   char temp1 = up[1];
   char temp2 = up[0];
@@ -64,6 +64,35 @@ void Cube::rotate_back() {
   left[0] = temp0;
   left[3] = temp1;
   left[6] = temp2;
+}
+
+void Cube::rotate_down() {
+  char temp = down[0];
+  down[0] = down[6];
+  down[6] = down[8];
+  down[8] = down[2];
+  down[2] = temp;
+  temp = down[1];
+  down[1] = down[3];
+  down[3] = down[7];
+  down[7] = down[5];
+  down[5] = temp;
+
+  char temp0 = front[6];
+  char temp1 = front[7];
+  char temp2 = front[8];
+  front[6] = left[6];
+  front[7] = left[7];
+  front[8] = left[8];
+  left[6] = back[6];
+  left[7] = back[7];
+  left[8] = back[8];
+  back[6] = right[6];
+  back[7] = right[7];
+  back[8] = right[8];
+  right[6] = temp0;
+  right[7] = temp1;
+  right[8] = temp2;
 }
 
 Cube::~Cube() {
