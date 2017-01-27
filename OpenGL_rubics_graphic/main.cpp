158e193f8fc29a4ef9fdda440196ad03d6fcfe14
Czetenyi Kristof <wekkew@gmail.com>
2017-01-27 15:49:01 +0100

    gitkurwa

--- OpenGL_rubics_graphic/main.cpp
+++ OpenGL_rubics_graphic/main.cpp
@@ -33,108 +33,127 @@ int which_button(int x, int y, vector<SDL_Rect> buttons) {
   }
 }
 
+void winner() {
+  SDL_Window* window = SDL_CreateWindow("winner", 0, 30, 640, 480, 0);
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+  SDL_Surface* surface = IMG_Load("youwon.jpg");
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  SDL_RenderPresent(renderer);
+}
+
 int main(int argc, char* argv[]) {
 
   Cube my_cube;
-  //kristof bindzsi eleje
-  Start_Window start_window;
-  SDL_Event event;
-  vector<SDL_Rect> buttons;
-  SDL_Rect easy = { 40, 130, 150, 50 };
-  buttons.push_back(easy);
-  SDL_Rect medium = { 240, 130, 150, 50 };
-  buttons.push_back(medium);
-  SDL_Rect hard = { 450, 130, 150, 50 };
-  buttons.push_back(hard);
 
-  bool start_running = true;
-  while (start_running) {
-    while (SDL_PollEvent(&event)) {
-      int x, y;
-      switch (event.type) {
-      case SDL_QUIT:
-        start_running = false;
-        break;
-      case SDL_KEYDOWN:
-        switch (event.key.keysym.sym) {
-        case SDLK_ESCAPE:
+  {
+    //kristof bindzsi eleje
+    Start_Window start_window;
+    SDL_Event event;
+    vector<SDL_Rect> buttons;
+    SDL_Rect easy = { 40, 130, 150, 50 };
+    buttons.push_back(easy);
+    SDL_Rect medium = { 240, 130, 150, 50 };
+    buttons.push_back(medium);
+    SDL_Rect hard = { 450, 130, 150, 50 };
+    buttons.push_back(hard);
+
+    bool start_running = true;
+    while (start_running) {
+      while (SDL_PollEvent(&event)) {
+        int x, y;
+        switch (event.type) {
+        case SDL_QUIT:
           start_running = false;
           break;
-        }
-      case SDL_MOUSEBUTTONDOWN:
-        SDL_GetMouseState(&x, &y);
-        switch (which_button(x, y, buttons)) {
-        case 0: {
-          cout << "easy" << endl;
-          SDL_DestroyWindow(start_window.window);
-          my_cube.random_shuffle(5);
-          start_running = false;
+        case SDL_KEYDOWN:
+          switch (event.key.keysym.sym) {
+          case SDLK_ESCAPE:
+            start_running = false;
+            break;
+          }
+        case SDL_MOUSEBUTTONDOWN:
+          SDL_GetMouseState(&x, &y);
+          switch (which_button(x, y, buttons)) {
+          case 0: {
+            cout << "easy" << endl;
+            SDL_DestroyWindow(start_window.window);
+            my_cube.random_shuffle(5);
+            start_running = false;
+            break;
+          }
+          case 1: {
+            cout << "medium" << endl;
+            //SDL_DestroyWindow(start_window.window);
+            my_cube.random_shuffle(10);
+            start_running = false;
+            break;
+          }
+          case 2: {
+            cout << "hard" << endl;
+            //SDL_DestroyWindow(start_window.window);
+            my_cube.random_shuffle(20);
+            start_running = false;
+            break;
+          }
+          }
           break;
         }
-        case 1: {
-          cout << "medium" << endl;
-          //SDL_DestroyWindow(start_window.window);
-          my_cube.random_shuffle(10);
-          start_running = false;
-          break;
-        }
-        case 2: {
-          cout << "hard" << endl;
-          //SDL_DestroyWindow(start_window.window);
-          my_cube.random_shuffle(20);
-          start_running = false;
-          break;
-        }
-        }
-        break;
       }
+      start_window.render();
     }
-    start_window.render();
+    //kristof binzsi vege
   }
-  //kristof binzsi vege
+  {
+    // lego bindzsije
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *screen;
+    screen = SDL_CreateWindow("Rubic's  window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, /*SDL_WINDOW_FULLSCREEN |*/ SDL_WINDOW_OPENGL);
+    SDL_GL_CreateContext(screen);
+    Opengl opgl;
+    Camera cam;
+    Cube_drawer primitive;
+    primitive.load_texture("pics/green.bmp");
+    primitive.load_texture("pics/red.bmp");
+    primitive.load_texture("pics/blue.bmp");
+    primitive.load_texture("pics/orange.bmp");
+    primitive.load_texture("pics/white.bmp");
+    primitive.load_texture("pics/yellow.bmp");
+    User_input ui(WIDTH, HEIGHT);
+    opgl.opengl_init(WIDTH, HEIGHT);
+    bool solver = false;
+    bool running = true;
+    while (running) {
+      ui.input_handler(running, solver, my_cube);
+      opgl.opengl_sreenbuilder();
+      cam.place_camera();
+      cam.rotate_camera(ui.get_angle_x(), ui.get_angle_z());
 
-  // lego bindzsije
-  SDL_Init(SDL_INIT_VIDEO);
-  SDL_Window *screen;
-  screen = SDL_CreateWindow("Rubic's  window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, /*SDL_WINDOW_FULLSCREEN |*/ SDL_WINDOW_OPENGL);
-  SDL_GL_CreateContext(screen);
-  Opengl opgl;
-  Camera cam;
-  Cube_drawer primitive;
-  primitive.load_texture("pics/green.bmp");
-  primitive.load_texture("pics/red.bmp");
-  primitive.load_texture("pics/blue.bmp");
-  primitive.load_texture("pics/orange.bmp");
-  primitive.load_texture("pics/white.bmp");
-  primitive.load_texture("pics/yellow.bmp");
-  User_input ui(WIDTH, HEIGHT);
-  opgl.opengl_init(WIDTH, HEIGHT);
-  bool solver = false;
-  bool running = true;
-  while (running) {
-    ui.input_handler(running, solver, my_cube);
-    opgl.opengl_sreenbuilder();
-    cam.place_camera();
-    cam.rotate_camera(ui.get_angle_x(), ui.get_angle_z());
+      if (solver) {
+        SDL_Delay(800);
+        my_cube.undo_last_step();
+        primitive.draw_real_cube(my_cube.get_vector_for_Lego(), 3);
+        if (my_cube.trackback.empty()) {
+          solver = false;
+        }
+      }
+      else {
+        primitive.draw_real_cube(my_cube.get_vector_for_Lego(), 3);
+      }
 
-    if (solver) {
-      SDL_Delay(800);
-      my_cube.undo_last_step();
-      primitive.draw_real_cube(my_cube.get_vector_for_Lego(), 3);
-      if (my_cube.trackback.empty()) {
-        solver = false;
+      primitive.draw_background();
+      opgl.opengl_display(screen);
+      if (my_cube.is_ready()) {
+        running = false;
       }
     }
-    else {
-      primitive.draw_real_cube(my_cube.get_vector_for_Lego(), 3);
-    }
-
-    
-    
-    primitive.draw_background();
-    opgl.opengl_display(screen);
-  } 
-  // lego bindzsi vege
+    // lego bindzsi vege
+  }
+  
+  //feco binzsi start
+  winner();
+  //feco binzsi end
 
   SDL_Quit();
   return 0;
